aa152e99c3a5f1eddc90ad3a30f3ed97a9e54113
monkey codes <jz@monkey.codes>
2017-02-01 18:03:36 +1100

    Small refactor

--- src/index.js
+++ src/index.js
@@ -2,20 +2,10 @@ import { Observable } from 'rxjs/Rx';
 import fs from 'fs';
 import recursiveReaddir from 'recursive-readdir';
 import _ from 'lodash';
-import colour from 'colour';
+import 'colour';
 
 // const DIR = '/Users/jzietsman/vagrant/devvm-0.5/git/stayz/stayz-core/src/main/java';
 const DIR = '/Users/jzietsman/tmp3';
-
-colour.setTheme({
-  after: 'green',
-  before: 'red',
-});
-
-function swapLeadingSpace(line) {
-  const match = /^\s+/.exec(line);
-  return match ? line.trim() + match[0] : line.trim();
-}
 
 const files$ = Observable.bindNodeCallback(recursiveReaddir)(DIR)
   .mergeMap(files => Observable.from(files));
@@ -24,24 +14,23 @@ const files$ = Observable.bindNodeCallback(recursiveReaddir)(DIR)
 files$
   .mergeMap(file => (
     Observable.bindNodeCallback(fs.readFile)(file)
-      .map(content => content.toString().split('\n'))
-      .mergeMap((lines) => {
-        const max$ = Observable.from(lines)
-          .map(line => line.length)
-          .max();
-        return Observable.of(lines)
-          .withLatestFrom(max$, (lines, max) => ({ file, lines, max }))
-          .mergeMap(({ file, lines, max }) => (
-            Observable.from(lines)
-              .map(swapLeadingSpace)
-              .map(line => _.padStart(line, max))
-              .toArray()
-              .map(indentedLines => ({ file, lines, max, indentedLines }))
-          ));
-      })
+    .map(content => ({ file, lines: content.toString().split('\n') }))
+  ))
+  .mergeMap(({ file, lines }) => (
+    Observable.from(lines)
+    .map(line => line.length)
+    .max()
+    .map(max => ({ file, lines, max }))
+  ))
+  .mergeMap(({ file, lines, max }) => (
+    Observable.from(lines)
+    .map(line => line.trim() + /^\s*/.exec(line)[0])
+    .map(line => _.padStart(line, max))
+    .toArray()
+    .map(indentedLines => ({ file, lines, max, indentedLines }))
   ))
   .do(({ file, lines, indentedLines }) => {
-    console.log(`\n${file}\n${lines.join('\n').before}\n${indentedLines.join('\n').after}\n`);
+    console.log(`\n${file}\n${lines.join('\n').red}\n${indentedLines.join('\n').green}\n`);
   })
   .subscribe(({ file, indentedLines }) => {
     fs.writeFile(file, indentedLines.join('\n'), (err) => {
