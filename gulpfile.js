db3ca8d121a3266cb6bb2a92ceec2d702978e505
monkey codes <jz@monkey.codes>
2017-02-01 15:55:50 +1100

    Include linting in the build process

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,29 @@ var gulp = require('gulp'),
   path = require('path'),
   fs = require('fs');
 
-gulp.task('build', () =>{
-  return gulp.src('./src/**/*.js')
-    .pipe($.cached('server'))
-    .pipe($.babel())
-    .pipe(gulp.dest('./build'))
-});
-
-gulp.task('watch:build', gulp.series(
-  'build',
-  () => gulp.watch('./src/**/*.js', gulp.series('build'))
-));
-
 gulp.task('lint', () => {
     return gulp.src(['./src/**/*.js'])
         .pipe($.eslint())
         .pipe($.eslint.format());
+        //.pipe($.eslint.failAfterError());
+
 });
 
 gulp.task('watch:lint', gulp.series(
   'lint',
   () => gulp.watch('./src/**/*.js', gulp.series('lint'))
 ));
+
+gulp.task('build', gulp.series('lint', () => {
+  return gulp.src('./src/**/*.js')
+    .pipe($.cached('server'))
+    .pipe($.babel())
+    .pipe(gulp.dest('./build'))
+}));
+
+gulp.task('watch:build', gulp.series(
+  'build',
+  () => gulp.watch('./src/**/*.js', gulp.series('build'))
+));
+
+
