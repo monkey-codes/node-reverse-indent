77eb2c5ccb2cd5f04155acf5a38f5439e3f26d77
monkey codes <jz@monkey.codes>
2017-02-09 10:23:45 +1100

    Converted into cli

--- src/index.js
+++ src/index.js
@@ -1,41 +1,48 @@
-import { Observable } from 'rxjs/Rx';
-import fs from 'fs';
-import recursiveReaddir from 'recursive-readdir';
-import _ from 'lodash';
-import 'colour';
+#!/usr/bin/env node
+import program  from 'commander';
+import rindent from './lib/rindent'
+
+program
+  .arguments('<dir>')
+  .option('-v, --verbose', 'Verbose output')
+  .option('-d, --dry-run', 'Prints result without changing the file')
+  .action(dir => {
+    rindent(dir, program);
+  })
+.parse(process.argv)
 
 // const DIR = '/Users/jzietsman/vagrant/devvm-0.5/git/stayz/stayz-core/src/main/java';
-const DIR = '/Users/jzietsman/tmp3';
-
-const files$ = Observable.bindNodeCallback(recursiveReaddir)(DIR)
-  .mergeMap(files => Observable.from(files));
-
-/* eslint-disable no-shadow*/
-files$
-  .mergeMap(file => (
-    Observable.bindNodeCallback(fs.readFile)(file)
-    .map(content => ({ file, lines: content.toString().split('\n') }))
-  ))
-  .mergeMap(({ file, lines }) => (
-    Observable.from(lines)
-    .map(line => line.length)
-    .max()
-    .map(max => ({ file, lines, max }))
-  ))
-  .mergeMap(({ file, lines, max }) => (
-    Observable.from(lines)
-    .map(line => line.trim() + /^\s*/.exec(line)[0])
-    .map(line => _.padStart(line, max))
-    .toArray()
-    .map(indentedLines => ({ file, lines, max, indentedLines }))
-  ))
-  .do(({ file, lines, indentedLines }) => {
-    console.log(`\n${file}\n${lines.join('\n').red}\n${indentedLines.join('\n').green}\n`);
-  })
-  .subscribe(({ file, indentedLines }) => {
-    fs.writeFile(file, indentedLines.join('\n'), (err) => {
-      if (err) throw err;
-      console.log(`${'saved'.green} ${file}`.green);
-    });
-  });
-
+//const DIR = '/Users/jzietsman/tmp3';
+//
+//const files$ = Observable.bindNodeCallback(recursiveReaddir)(DIR)
+//  .mergeMap(files => Observable.from(files));
+//
+///* eslint-disable no-shadow*/
+//files$
+//  .mergeMap(file => (
+//    Observable.bindNodeCallback(fs.readFile)(file)
+//    .map(content => ({ file, lines: content.toString().split('\n') }))
+//  ))
+//  .mergeMap(({ file, lines }) => (
+//    Observable.from(lines)
+//    .map(line => line.length)
+//    .max()
+//    .map(max => ({ file, lines, max }))
+//  ))
+//  .mergeMap(({ file, lines, max }) => (
+//    Observable.from(lines)
+//    .map(line => line.trim() + /^\s*/.exec(line)[0])
+//    .map(line => _.padStart(line, max))
+//    .toArray()
+//    .map(indentedLines => ({ file, lines, max, indentedLines }))
+//  ))
+//  .do(({ file, lines, indentedLines }) => {
+//    console.log(`\n${file}\n${lines.join('\n').red}\n${indentedLines.join('\n').green}\n`);
+//  })
+//  .subscribe(({ file, indentedLines }) => {
+//    fs.writeFile(file, indentedLines.join('\n'), (err) => {
+//      if (err) throw err;
+//      console.log(`${'saved'.green} ${file}`.green);
+//    });
+//  });
+//
---
+++ src/lib/rindent.js
@@ -0,0 +1,48 @@
+import { Observable } from 'rxjs/Rx';
+import fs from 'fs';
+import recursiveReaddir from 'recursive-readdir';
+import _ from 'lodash';
+import 'colour';
+
+export default function rindent(dir, args) {
+  const files$ = Observable.bindNodeCallback(recursiveReaddir)(dir)
+    .mergeMap(files => Observable.from(files));
+
+  /* eslint-disable no-shadow*/
+  files$
+    .mergeMap(file => (
+      Observable.bindNodeCallback(fs.readFile)(file)
+      .map(content => ({ file, lines: content.toString().split('\n') }))
+    ))
+    .mergeMap(({ file, lines }) => (
+      Observable.from(lines)
+      .map(line => line.length)
+      .max()
+      .map(max => ({ file, lines, max }))
+    ))
+    .mergeMap(({ file, lines, max }) => (
+      Observable.from(lines)
+      .map(line => line.trim() + /^\s*/.exec(line)[0])
+      .map(line => _.padStart(line, max))
+      .toArray()
+      .map(indentedLines => ({ file, lines, max, indentedLines }))
+    ))
+    .do(({ file, lines, indentedLines }) => {
+      console.log(`${'reverse indented:'.green} ${file}`);
+      if (args.verbose) {
+        console.log(`\n${lines.join('\n').red}\n${indentedLines.join('\n').green}\n`);
+      }
+    })
+    .subscribe(({ file, indentedLines }) => {
+      if (args.dryRun) {
+        console.log(`\n${indentedLines.join('\n').red}`);
+        return;
+      }
+
+      fs.writeFile(file, indentedLines.join('\n'), (err) => {
+        if (err) throw err;
+        console.log(`${'saved'.green} ${file}`.green);
+      });
+
+    });
+}
